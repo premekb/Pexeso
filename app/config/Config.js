5054e39c8139a5410cb8dad95fb560b131e282d3
premekb <38076249+premekb@users.noreply.github.com>
2022-04-18 16:57:55 +0200

    serviceWorker

--- app/config/Config.js
+++ app/config/Config.js
@@ -1,3 +1,3 @@
 export default class Config{
-    static CARDS_IN_GAME = 4 // Must be divisible by two
+    static CARDS_IN_GAME = 20 // MAX: 20, Must be divisible by two
 }
--- app/sw.js
+++ app/sw.js
@@ -1,63 +1,99 @@
 /**
- * Heavily inspired by https://ondras.github.io/rri/sw.js
+ * I used cache-only strategy with slight changes from the cookbook shown in a lecture.
+ *
+ * https://serviceworke.rs/strategy-cache-only.html
+ * https://github.com/mdn/serviceworker-cookbook/tree/master/strategy-cache-only
+ *
+ * If I were using NPM, then I would have opted for the sw-precache project (https://stackoverflow.com/questions/45222450/bulk-add-all-js-or-css-files-to-service-worker-cache-on-install)
+ * Which would allow me to add all files in the project in a bulk, instead of having them listed out one by one
+ * as I have it now.
  */
 
- const CACHE_NAME = "offline";
- const urlsToCache = [
-     "app.js",
-     "General.css",
-     "app.html",
-     "util/HistoryHandler.js",
-     "util/LocalStorageKeys.js",
-     "service/BoardService.js",
-     "service/CardService.js",
-     "service/GameResultService.js",
-     "service/GameService.js",
-     "resources/card_flip.wav",
-     "resources/ding.wav",
-     "pages/AbstractPage.js",
-     "pages/GameEndPage.js",
-     "pages/GamePage.js",
-     "pages/HighScorePage.js",
-     "pages/MainMenuPage.js",
-     "model/Board.js",
-     "model/Card.js",
-     "model/Game.js",
-     "model/GameResult.js",
-     "factory/GameFactory.js"
- ];
- 
- async function precache() {
-	const cache = await caches.open(CACHE_NAME);
-	return cache.addAll(urlsToCache);
-};
+var CACHE = 'cache-only';
 
-async function respondTo(request) {
-    const cached = await caches.match(request);
-    let f = fetch(request);
+// On install, cache some resources.
+self.addEventListener('install', function(evt) {
+    console.log('The service worker is being installed.');
 
-	if (cached) { // try updating the cache first
-		try {
-			let response = await f;
-			let cache = await caches.open(CACHE_NAME);
-			cache.put(request, response.clone());
-			return response;
-		} catch (e) { // offline
-			return cached;
-		}
-	} else { // not cached, forward to network
-		return f;
-	}
-};
+    // Ask the service worker to keep installing until the returning promise
+    // resolves.
+    evt.waitUntil(precache());
+});
 
-async function onInstall(e) {
-	self.skipWaiting();
-	e.waitUntil(precache());
+// On fetch, use cache only strategy.
+self.addEventListener('fetch', function(evt) {
+    console.log('The service worker is serving the asset.');
+    evt.respondWith(fromCache(evt.request));
+});
+
+// Open a cache and use `addAll()` with an array of assets to add all of them
+// to the cache. Return a promise resolving when all the assets are added.
+function precache() {
+    return caches.open(CACHE).then(function (cache) {
+        return cache.addAll(
+            URLS_TO_CACHE
+        );
+    });
 }
 
-async function onFetch(e) {
-	e.respondWith(respondTo(e.request));
+// Open the cache where the assets were stored and search for the requested
+// resource. Notice that in case of no matching, the promise still resolves
+// but it does with `undefined` as value.
+function fromCache(request) {
+    return caches.open(CACHE).then(function (cache) {
+        return cache.match(request, {cacheName:CACHE, ignoreVary:true, ignoreSearch:true}).then(function (matching) {
+            return matching || Promise.reject('no-match');
+        });
+    });
 }
 
-self.addEventListener("install", onInstall);
-self.addEventListener("fetch", onFetch);
+const URLS_TO_CACHE = [
+    "./util/ClickedCardsContainer.js",
+    "./util/TimeConverter.js",
+    "./util/CanvasHelper.js",
+    "./util/LocalStorageKeys.js",
+    "./config/Config.js",
+    "./resources/img/no_wifi.svg",
+    "./resources/img/cards/card_8.svg",
+    "./resources/img/cards/card_9.svg",
+    "./resources/img/cards/card_10.svg",
+    "./resources/img/cards/card_7.svg",
+    "./resources/img/cards/card_6.svg",
+    "./resources/img/cards/card_4.svg",
+    "./resources/img/cards/card_5.svg",
+    "./resources/img/cards/card_1.svg",
+    "./resources/img/cards/card_2.svg",
+    "./resources/img/cards/card_3.svg",
+    "./resources/img/wifi.svg",
+    "./resources/readme.txt",
+    "./resources/font/magic.ttf",
+    "./resources/font/fantini.ttf",
+    "./resources/sound/sound_icon.png",
+    "./resources/sound/no_sound.png",
+    "./resources/sound/card_flip.wav",
+    "./resources/sound/ding.wav",
+    "./styles/GamePage.css",
+    "./styles/GameEndPage.css",
+    "./styles/HighScorePage.css",
+    "./styles/General.css",
+    "./styles/MainMenuPage.css",
+    "./model/Game.js",
+    "./model/GameResult.js",
+    "./model/Board.js",
+    "./model/Card.js",
+    "./app.html",
+    "./service/other/FileHandler.js",
+    "./service/other/HistoryHandler.js",
+    "./service/game/GameResultService.js",
+    "./service/game/GameService.js",
+    "./service/game/CardService.js",
+    "./service/game/BoardService.js",
+    "./pages/GameEndPage.js",
+    "./pages/HighScorePage.js",
+    "./pages/AbstractPage.js",
+    "./pages/MainMenuPage.js",
+    "./pages/GamePage.js",
+    "./factory/GameFactory.js",
+    "./app.js",
+    "./sw.js"
+]
